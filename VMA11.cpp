e952ff75ba714bc90b6602b47d1e6d9e08f1e3c5
Brecht Nuyttens <bn@velleman.be>
2016-06-10 10:05:06 +0200

    V1.0
    Release Version

--- VMA11.cpp
+++ VMA11.cpp
@@ -1,86 +1,149 @@
-/* Arduino Si4735 Library
-* Written by Ryan Owens for SparkFun Electronics
-* 5/17/11
-*
-* This library is for use with the SparkFun Si4735 Shield
-* Released under the 'Buy Me a Beer' license
-* (If we ever meet, you buy me a beer)
-*
-* See the header file for better function documentation.
-*
-* See the example sketches to learn how to use the library in your code.
-*/
-
+#include "Arduino.h"
 #include "VMA11.h"
 
 
-//This is just a constructor.
-VMA11::VMA11(){
-	//Do Nothing
+VMA11::VMA11(int resetPin, int sdioPin, int sclkPin) : Si4703_Breakout(resetPin,sdioPin,sclkPin)
+{
+  
 }
 
+int ReadRDSState = 0;
+#define CLEARBUFFER 0
+#define READUNTILEOL 1
+#define READUNTILBEGIN 2
+#define READUNTILEND 3
+uint8_t position = 0;
+char poststation[9];
 
-void VMA11::begin(){
-	typFREQ freq;
-	freq.word = 10210;
-	StartingSequence(freq);
+char VMA11::readRDSRadioStation(char* rs)
+{
+	strcpy(rs,poststation);
 }
 
-void VMA11::sendCommand(char * myCommand){
+char VMA11::readRDSRadioText(char* rt)
+{	
+	memset(poststation,0,9);
+	char status = 0;
+	switch(ReadRDSState)
+	{
+		case CLEARBUFFER:
+		memset(rt,0,65);
+		memset(radiotext,0,65);
+		
+			ReadRDSState++;
+		break;
+		case READUNTILEOL:
+			if(strchr(rt,0x0D) == 0)//When no CR(Carriage Return) is found => read RDS
+			{
+				readRDS(poststation,rt,&position);
+			}
+			else
+			{
+				memset(rt,0,65);
+				memset(radiotext,0,65);
+				ReadRDSState++;
+				position = 100;
+			}
+		break;
+		case READUNTILBEGIN:			
+			if(position)
+			{
+				readRDS(poststation,rt,&position);
+			}
+			else
+			{
+				ReadRDSState++;
+				position = 100;
+				memset(rt,0,65);
+				memset(radiotext,0,65);
+			}
+		break;
+		case READUNTILEND:
+			if(strchr(rt,0x0D) == 0)
+			{
+				readRDS(poststation,rt,&position);
+			}
+			else
+			{	
+				if(CheckValidText(rt))
+				{
+					status = 1; //Text found
+					ReadRDSState = 0;
+				}		
+			}
+		break;
+		default:
+		break;
+		
+	}
+	return status;
+}
+
+static inline void considerrdschar(char *buf, int place, char ch)
+{	
+		if (ch < 0x10 || ch > 0x7F)
+		{
+			if(ch != 0x0D)
+				return;
+		}
+		buf[place] = ch;
+}
+
+//void Si4703_Breakout::readRDS(char* buffer, long timeout)
+int VMA11::readRDS(char* ps, char* rt,uint8_t* pos)
+{ 
+	readRegisters();
+	if(si4703_registers[STATUSRSSI] & (1<<RDSR)) {
+		if (fakerds) {
+			memset(rdsdata, 0, 8);
+			rdschanged = 1;
+		}
+		
+		fakerds = 0;
+		
+		//const uint16_t a = si4703_registers[RDSA];
+		const uint16_t b = si4703_registers[RDSB];
+		const uint16_t c = si4703_registers[RDSC];
+		const uint16_t d = si4703_registers[RDSD];
+		
+		const uint8_t groupid = (b & 0xF000) >> 12;
+		uint8_t version = b & 0x10;
+		
+		switch(groupid) {
+			case RDS_PS: {
+				const uint8_t index = (b & 0x3)*2;
+				char Dh = (d & 0xFF00) >> 8;
+				char Dl = d;
+				
+				considerrdschar(rdsdata, index, Dh);
+				considerrdschar(rdsdata, index +1, Dl);
+				
+				rdschanged = 1;
+			};
+				break;
+			case RDS_RT: {
+				rdschanged = 1;
+				uint8_t index = (b & 0xF)*4;
+				*pos = index; 
+				char Ch = (c & 0xFF00) >> 8;
+				char Cl = c;
+				char Dh = (d & 0xFF00) >> 8;
+				char Dl = d;
+				
+				considerrdschar(radiotext, index, Ch);
+				considerrdschar(radiotext, index +1, Cl);
+				considerrdschar(radiotext, index +2, Dh);
+				considerrdschar(radiotext, index +3, Dl);
+			};
+				break;
+		}
+	}
 	
+	const int change = rdschanged;
+	if (change) {
+		strcpy(ps, rdsdata);
+		strcpy(rt, radiotext);
+	}
+	rdschanged = 0;
+	return (change) ? ((fakerds) ? (RDS_FAKE) : (RDS_AVAILABLE)) : (RDS_NO);
 }
-
-/*
-* Description: Tunes the Si4735 to a frequency
-*
-* Params: int frequency - The desired frequency in kHz
-*
-* Returns: True if tune was successful
-*			False if tune was unsuccessful
-*/
-bool VMA11::tuneFrequency(int frequency){
-	
-}
-
-//This function does not work yet!
-int VMA11::getFrequency(void){
-	return 0;
-}
-
-bool VMA11::seekUp(void){
-	
-}
-
-bool VMA11::seekDown(void){
-	
-}
-
-void VMA11::volumeUp(void){
-	
-}
-
-void VMA11::volumeDown(void){
-	
-}
-
-void VMA11::mute(void){
-	
-}
-
-void VMA11::unmute(void){
-	
-}
-
-char VMA11::getStatus(void){
-	
-}
-void VMA11::getResponse(char * response){
-	
-}
-
-void VMA11::end(void){
-	
-}
-
-
-
--- VMA11.h
+++ VMA11.h
@@ -1,119 +1,13 @@
-// Velleman VMA11 Code by BN
-// Released to the public domain! Enjoy!
-#ifndef VMA11_H
-#define VMA11_H
-
-#include <5B700.h>
-
-//Define the SPI Pin Numbers
-//#define SDA 18		//MOSI
-//#define SCL  19		//MISO 
-//#define ADDRESS 100	//Chips address
-
-#define ON	true
-#define OFF	false
-
-// Main Class for VMA11 FM radio shield
-class VMA11 {
-
-#pragma region Public Methods
-public:
-	//This is just a constructor.
-	VMA11();
-	/*
-	* Description:
-	*	Initializes the 5B700, powers up the radio.
-	* 	This function must be called before any other radio command.
-	*/
-	void begin();
-	/*
-	* Description:
-	*	Used to send an ascii command string to the radio.
-	* Parameters:
-	*	myCommand - A null terminated ascii string limited to hexidecimal characters
-	*				to be sent to the radio module. Instructions for building commands can be found
-	*				in the Si4735 Programmers Guide.
-	*/
-	void sendCommand(char * myCommand);
-	/*
-	* Description:
-	*	Used to to tune the radio to a desired frequency. The library uses the mode indicated in the
-	* 	begin() function to determine how to set the frequency.
-	* Parameters:
-	*	frequency - The frequency to tune to, in kHz (or in 10kHz if using FM mode).
-	* Returns:
-	*	True
-	* TODO:
-	* 	Make the function return true if the tune was successful, else return false.
-	*/
-	bool tuneFrequency(int frequency);
-	/*
-	* Description:
-	*	This function currently does not work!
-	* TODO:
-	*	Make this function work.
-	*/
-	int getFrequency(void);
-	/*
-	* Description:
-	*	Commands the radio to seek up to the next valid channel. If the top of the band is reached, the seek
-	*	will continue from the bottom of the band.
-	* Returns:
-	*	True
-	* TODO:
-	*	Make the function return true if a valid channel was found, else return false.
-	*/
-	bool seekUp(void);
-	/*
-	* Description:
-	*	Commands the radio to seek down to the next valid channel. If the bottom of the band is reached, the seek
-	*	will continue from the top of the band.
-	* Returns:
-	*	True
-	* TODO:
-	*	Make the function return true if a valid channel was found, else return false.
-	*/
-	bool seekDown(void);
-	/*
-	* Description:
-	*	Increasese the volume by 1. If the maximum volume has been reached, no increase will take place.
-	*/
-	void volumeUp(void);
-	/*
-	* Description:
-	*	Decreases the volume by 1. If the minimum volume has been reached, no decrease will take place.
-	*/
-	void volumeDown(void);
-	/*
-	* Description:
-	*	Mutes the audio output
-	*/
-	void mute(void);
-	/*
-	* Description:
-	*	Disables the mute.
-	*/
-	void unmute(void);
-	/*
-	* Description:
-	*	Gets the current status of the radio. Learn more about the status in the Si4735 datasheet.
-	* Returns:
-	*	The status of the radio.
-	*/
-	char getStatus(void);
-	/*
-	* Description:
-	*	Gets the long response (16 characters) from the radio. Learn more about the long response in the Si4735 datasheet.
-	* Parameters:
-	*	response - A string for the response from the radio to be stored in.
-	*/
-	void getResponse(char * response);
-	/*
-	* Description:
-	*	Powers down the radio
-	*/
-	void end(void);
-#pragma endregion
-
-};
+#ifndef __VMA11_H__
+#define __VMA11_H__
 #endif
+#include "Si4703_Breakout_Modified.h"
+#include "Arduino.h"
+class VMA11 : public Si4703_Breakout
+{
+	public:
+		VMA11(int resetPin, int sdioPin, int sclkPin);
+		int readRDS(char* ps, char* rt,uint8_t* pos);
+		char readRDSRadioText(char* rt);
+		char readRDSRadioStation(char* rs);
+};
